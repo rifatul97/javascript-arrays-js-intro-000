81c97839eac3f6d6a1599546bbb65d6e6089b1fb
Rifatul Karim <rifatulkarim1997@gmail.com>
2020-05-15 19:35:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,12 @@
-function addElementToBeginningOfArray(arr, x){
-  new_arr = arr;
-  new_arr.unshift(x);
-  return new_arr;
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(arr, a){
-  return arr.unshift(a)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(arr, y){
